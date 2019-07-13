2840d0620a05caf94cc0b3028acbe9832b07ccac
Недогорская Татьяна <nedogora@gmail.com>
2019-07-13 09:51:55 +0300

    methods_out_of_class

--- String/main.cpp
+++ String/main.cpp
@@ -12,27 +12,48 @@ class String
 	int size;	//размер строки.
 	char* str;	//адрес строки в динамической памяти.
 public:
-	const int get_size() const
+	const int get_size() const;
+	const char* get_str() const;
+	char* get_str();
+
+	//			Constructors:
+	String(int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other);
+	~String();
+
+	//			Operators:
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
+	const char& operator[](int i) const;
+	char& operator[](int i);
+
+	//			Methods:
+	void print();
+};
+	const int String::get_size() const
 	{
 		return size;
 	}
-	const char* get_str() const
+	const char* String::get_str() const
 	{
 		return str;
 	}
-	char* get_str()
+	char* String::get_str()
 	{
 		return str;
 	}
 
 	//			Constructors:
-	String(int size = 80)
+	String::String(int size)
 	{
 		this->size = size;
 		this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char str[])
+	String::String(const char str[])
 	{
 		this->size = strlen(str) + 1;
 		this->str = new char[size] {};
@@ -42,7 +63,7 @@ public:
 		}
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other)
+	String::String(const String& other)
 	{
 		this->size = other.size;
 		this->str = new char[size] {};
@@ -52,21 +73,21 @@ public:
 		}
 		cout << "CopyConstructor:" << this << endl;
 	}
-	String(String&& other)
+	String::String(String&& other)
 	{
 		this->size = other.size;
 		this->str = other.str;
 		other.str = nullptr;
 		cout << "MoveConstructor:" << this << endl;
 	}
-	~String()
+	String::~String()
 	{
 		delete[] this->str;
 		cout << "Destructor:\t" << this << endl;
 	}
 
 	//			Operators:
-	String& operator=(const String& other)
+	String& String::operator=(const String& other)
 	{
 		//0)Проверяем, является ли тот объект этим объектом:
 		if (this == &other)return *this;
@@ -85,7 +106,7 @@ public:
 		return *this;
 	}
 
-	String& operator=(String&& other)
+	String& String::operator=(String&& other)
 	{
 		delete[] this->str;
 		this->size = other.size;
@@ -95,30 +116,29 @@ public:
 		return *this;
 	}
 
-	String& operator+=(const String& other)
+	String& String::operator+=(const String& other)
 	{
 		return *this = *this + other;
 	}
 
-	const char& operator[](int i) const
+	const char& String::operator[](int i) const
 	{
 		//char value = this->str[i];
 		return this->str[i];
 	}
 
-	char& operator[](int i)
+	char& String::operator[](int i)
 	{
 		//char value = this->str[i];
 		return this->str[i];
 	}
 
 	//			Methods:
-	void print()
+	void String::print()
 	{
 		cout << "size:\t" << size << endl;
 		cout << "str:\t" << str << endl;
 	}
-};
 
 String operator+(const String& left, const String& right)
 {
